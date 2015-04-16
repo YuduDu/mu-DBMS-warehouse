036187fb6dbdb4eba68aed568ad337f09880c7dc
YuduDu <dyd12315@163.com>
2015-04-16 00:35:22 -0500

    数据库更新

--- 2015_04_16数据库创建.sql
+++ 2015_04_16数据库创建.sql
@@ -25,7 +25,7 @@ DROP TABLE IF EXISTS Inner_Trasition ;
 -- Table Customers
 
 CREATE TABLE IF NOT EXISTS Customers (
-  Cid CHAR(10) NOT NULL,
+  Cid INT NOT NULL AUTO_INCREMENT,
   Cname VARCHAR(50) NOT NULL,
   Ccontact VARCHAR(50) NULL DEFAULT NULL,
   Caddress VARCHAR(500) NULL DEFAULT NULL,
@@ -47,7 +47,7 @@ ENGINE = InnoDB;
 -- Table Staffs
 
 CREATE TABLE IF NOT EXISTS Staffs (
-  Sid CHAR(10) NOT NULL,
+  Sid INT NOT NULL AUTO_INCREMENT,
   Sname VARCHAR(20) NOT NULL,
   SCid INT NULL DEFAULT NULL,
   PRIMARY KEY (Sid),
@@ -62,8 +62,8 @@ ENGINE = InnoDB;
 -- Table Warehouses
 
 CREATE TABLE IF NOT EXISTS Warehouses (
-  Wid INT NOT NULL,
-  Admin_id CHAR(10) NULL DEFAULT NULL,
+  Wid INT NOT NULL AUTO_INCREMENT,
+  Admin_id INT NULL DEFAULT NULL,
   PRIMARY KEY (Wid),
   INDEX Admin_idx (Admin_id ASC),
   CONSTRAINT Warehouses_Admin_id
@@ -76,16 +76,14 @@ ENGINE = InnoDB;
 -- Table Outbound
 
 CREATE TABLE IF NOT EXISTS Outbound (
-  Outbound_id CHAR(15) NOT NULL,
-  Customer_Cid CHAR(10) NULL DEFAULT NULL,
-  Createtime DATETIME NULL DEFAULT NULL,
-  Approver_id CHAR(10) NULL DEFAULT NULL,
+  Outbound_id INT NOT NULL AUTO_INCREMENT,
+  Customer_Cid INT NULL DEFAULT NULL,
+  Createtime DATETIME NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  Approver_id INT NULL DEFAULT NULL,
   Consignee VARCHAR(20) NULL DEFAULT NULL,
-  Wid INT NULL,
   PRIMARY KEY (Outbound_id),
   INDEX Cid_idx (Customer_Cid ASC),
   INDEX Approver_idx (Approver_id ASC),
-  INDEX Outbound_Warehouse_Wid_idx (Wid ASC),
   CONSTRAINT Outbound_Cid
     FOREIGN KEY (Customer_Cid)
     REFERENCES Customers (Cid)
@@ -95,24 +93,17 @@ CREATE TABLE IF NOT EXISTS Outbound (
     FOREIGN KEY (Approver_id)
     REFERENCES Staffs (Sid)
     ON DELETE NO ACTION
-    ON UPDATE CASCADE,
-  CONSTRAINT Outbound_Wid
-    FOREIGN KEY (Wid)
-    REFERENCES Warehouses (Wid)
-    ON DELETE NO ACTION
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
 
 -- Table Customer_Order_statistics
 
 CREATE TABLE IF NOT EXISTS Customer_Order_statistics (
-  Cid CHAR(10) NOT NULL,
-  Customer_Order_Index INT NOT NULL AUTO_INCREMENT,
-  Completetime DATETIME NULL,
+  Cid INT NOT NULL,
   Outbound_Amount FLOAT NULL,
   Money DECIMAL(10,2) NULL,
-  Outbound_id CHAR(15) NULL,
-  PRIMARY KEY (Customer_Order_Index),
+  Outbound_id INT NULL,
+  PRIMARY KEY (Outbound_id),
   INDEX Customers1_Cid (Cid ASC),
   INDEX Outbound_id (Outbound_id ASC),
   CONSTRAINT Cid
@@ -120,7 +111,7 @@ CREATE TABLE IF NOT EXISTS Customer_Order_statistics (
     REFERENCES Customers (Cid)
     ON DELETE NO ACTION
     ON UPDATE NO ACTION,
-  CONSTRAINT Customer_Outbound_id
+  CONSTRAINT Outbound_id
     FOREIGN KEY (Outbound_id)
     REFERENCES Outbound (Outbound_id)
     ON DELETE NO ACTION
@@ -130,24 +121,22 @@ ENGINE = InnoDB;
 -- Table Item Category
 
 CREATE TABLE IF NOT EXISTS Item_Category (
-  ICid VARCHAR(5) NOT NULL,
   ICname VARCHAR(50) NOT NULL,
-  PRIMARY KEY (ICid))
+  Spec VARCHAR(45) NULL,
+  PRIMARY KEY (ICname))
 ENGINE = InnoDB;
 
 -- Table Items
 
 CREATE TABLE IF NOT EXISTS Items (
-  Iid CHAR(15) NOT NULL,
-  ICid VARCHAR(5) NOT NULL,
   Iname VARCHAR(100) NOT NULL,
-  Spec VARCHAR(50) NULL DEFAULT NULL,
+  ICname VARCHAR(50) NULL,
   Unit VARCHAR(20) NULL DEFAULT NULL,
-  PRIMARY KEY (Iid, ICid),
-  INDEX Pcid_idx (ICid ASC),
+  PRIMARY KEY (Iname, ICname),
+  INDEX Pcid_idx (Iname ASC),
   CONSTRAINT Items_ICid
-    FOREIGN KEY (ICid)
-    REFERENCES Item_Category (ICid)
+    FOREIGN KEY (ICname)
+    REFERENCES Item_Category (ICname)
     ON DELETE NO ACTION
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
@@ -155,41 +144,46 @@ ENGINE = InnoDB;
 -- Table Outbound_details
 
 CREATE TABLE IF NOT EXISTS Outbound_details (
-  Outbound_d_id CHAR(15) NOT NULL,
-  Outbound_id CHAR(15) NOT NULL,
-  Outbound_Iid CHAR(15) NOT NULL,
+  Outbound_id INT NOT NULL,
+  Outbound_Iname VARCHAR(100) NOT NULL,
   Amount FLOAT NULL DEFAULT NULL,
-  Completetime DATETIME NULL DEFAULT NULL,
   Unit_price DECIMAL(10,2) NULL,
-  PRIMARY KEY (Outbound_d_id),
+  Warehouse_Wid INT NOT NULL,
+  PRIMARY KEY (Outbound_Iname, Outbound_id),
   INDEX Outbound_id_idx (Outbound_id ASC),
-  INDEX Outbound_Iid_idx (Outbound_Iid ASC),
-  CONSTRAINT Outbound_id
+  INDEX Outbound_Iname_idx (Outbound_Iname ASC),
+  INDEX Outbound_Warehouse_idx (Warehouse_Wid ASC),
+  CONSTRAINT D_Outbound_id
     FOREIGN KEY (Outbound_id)
     REFERENCES Outbound (Outbound_id)
     ON DELETE NO ACTION
     ON UPDATE CASCADE,
-  CONSTRAINT Outbound_Iid
-    FOREIGN KEY (Outbound_Iid)
-    REFERENCES Items (Iid)
+  CONSTRAINT D_Outbound_Iname
+    FOREIGN KEY (Outbound_Iname)
+    REFERENCES Items (Iname)
     ON DELETE NO ACTION
-    ON UPDATE CASCADE)
+    ON UPDATE CASCADE,
+  CONSTRAINT Outbound_Warehouse_Wid
+  	FOREIGN KEY (Warehouse_Wid)
+  	REFERENCES Warehouses (Wid)
+  	ON DELETE NO ACTION
+  	ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 -- Table Stock_collection
 
 CREATE TABLE IF NOT EXISTS Stock_collection (
   Remain_Amount FLOAT NOT NULL,
-  Iid CHAR(15) NOT NULL,
+  Items_Iname VARCHAR(100) NOT NULL,
   Wid INT NULL,
-  PRIMARY KEY (Iid),
+  PRIMARY KEY (Items_Iname),
   Minimum FLOAT NOT NULL,
   Maximum FLOAT NOT NULL,
-  INDEX Stock_collection_Items1_idx (Iid ASC),
+  INDEX Stock_collection_Items1_idx (Items_Iname ASC),
   INDEX Stock_collection_Wid_idx (Wid ASC),
-  CONSTRAINT Stock_collection_lid
-    FOREIGN KEY (Iid)
-    REFERENCES Items (Iid)
+  CONSTRAINT Stock_collection_Iname
+    FOREIGN KEY (Items_Iname)
+    REFERENCES Items (Iname)
     ON DELETE NO ACTION
     ON UPDATE CASCADE,
   CONSTRAINT Stock_collection_Wid
@@ -202,23 +196,23 @@ ENGINE = InnoDB;
 -- Table Stocks
 
 CREATE TABLE IF NOT EXISTS Stocks (
-  Stockid CHAR(20) NOT NULL,
+  Stockid INT NOT NULL AUTO_INCREMENT,
   Stocks_Wid INT NOT NULL,
-  Stocks_Iid CHAR(15) NOT NULL,
+  Stocks_Iname VARCHAR(100) NOT NULL,
   Stockamount FLOAT NOT NULL,
   Stockarea INT NULL DEFAULT NULL,
-  Instocktime DATETIME NULL DEFAULT NULL,
+  Instocktime DATETIME NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (Stockid),
   INDEX Wid_idx (Stocks_Wid ASC),
-  INDEX Pid_idx (Stocks_Iid ASC),
+  INDEX Pid_idx (Stockid ASC),
   CONSTRAINT Stocks_Wid
     FOREIGN KEY (Stocks_Wid)
     REFERENCES Warehouses (Wid)
     ON DELETE NO ACTION
     ON UPDATE CASCADE,
-  CONSTRAINT Stocks_Iid
-    FOREIGN KEY (Stocks_Iid)
-    REFERENCES Items (Iid)
+  CONSTRAINT Stocks_Iname
+    FOREIGN KEY (Stocks_Iname)
+    REFERENCES Items (Iname)
     ON DELETE NO ACTION
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
@@ -226,7 +220,7 @@ ENGINE = InnoDB;
 -- Table Suppliers
 
 CREATE TABLE IF NOT EXISTS Suppliers (
-  Sid CHAR(10) NOT NULL,
+  Sid INT NOT NULL AUTO_INCREMENT,
   Sname VARCHAR(50) NOT NULL,
   Scontact VARCHAR(50) NULL DEFAULT NULL,
   Saddress VARCHAR(500) NULL DEFAULT NULL,
@@ -240,10 +234,10 @@ ENGINE = InnoDB;
 -- Table Inbound
 
 CREATE TABLE IF NOT EXISTS Inbound (
-  Inbound_id CHAR(15) NOT NULL,
-  CreateTime DATETIME NULL DEFAULT NULL,
-  Approver_id CHAR(10) NULL DEFAULT NULL,
-  Suppliers_Sid CHAR(10) NOT NULL,
+  Inbound_id INT NOT NULL AUTO_INCREMENT,
+  CreateTime DATETIME NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  Approver_id INT NULL DEFAULT NULL,
+  Suppliers_Sid INT NOT NULL,
   Deliverer VARCHAR(20) NULL,
   PRIMARY KEY (Inbound_id),
   INDEX Approver_idx (Approver_id ASC),
@@ -263,13 +257,11 @@ ENGINE = InnoDB;
 -- Table Suppliers_Order_statistics
 
 CREATE TABLE IF NOT EXISTS Suppliers_Order_statistics (
-  Sid CHAR(10) NOT NULL,
-  Completetime DATETIME NULL DEFAULT NULL,
-  Inbound_Amount FLOAT NULL,
+  Sid INT NOT NULL,
+  CreateTime DATETIME NULL DEFAULT NULL,
   Money DECIMAL(10,2) NULL,
-  Suppliers_Order_index INT NOT NULL AUTO_INCREMENT,
-  Inbound_id CHAR(15) NULL,
-  PRIMARY KEY (Suppliers_Order_index),
+  Inbound_id INT NULL,
+  PRIMARY KEY (Inbound_id),
   INDEX Sid (Sid ASC),
   INDEX fk_Suppliers_Order_statistics_Inbound1_idx (Inbound_id ASC),
   CONSTRAINT Inblound_id
@@ -289,22 +281,27 @@ ENGINE = InnoDB;
 CREATE TABLE IF NOT EXISTS User (
   Username VARCHAR(20) NOT NULL,
   Password VARCHAR(20) NOT NULL,
-  PRIMARY KEY (Username))
+  Staffs_Sid int NOT NULL,
+  PRIMARY KEY (Username),
+  INDEX User_Staff_idx (Staffs_Sid ASC),
+  CONSTRAINT User_Staff
+  	FOREIGN KEY (Staffs_Sid)
+  	REFERENCES Staffs (sid)
+  	ON DELETE NO ACTION
+  	ON UPDATE NO ACTION)
 ENGINE = InnoDB;
 
 -- Table Inbound_details
 
 CREATE TABLE IF NOT EXISTS Inbound_details (
-  Inbound_d_id CHAR(15) NOT NULL,
-  Inbound_id CHAR(15) NOT NULL,
-  Inbound_Iid CHAR(15) NOT NULL,
+  Inbound_id INT NOT NULL,
+  Inbound_Iname VARCHAR(100) NOT NULL,
   Amount FLOAT NULL DEFAULT NULL,
   Unit_Price DECIMAL(10,2) NULL DEFAULT NULL,
-  Inbound_Stockid CHAR(20) NULL,
-  Completetime DATETIME NULL,
-  PRIMARY KEY (Inbound_d_id),
+  Inbound_Stockid INT NULL,
+  PRIMARY KEY (Inbound_id,Inbound_Iname),
   INDEX Inbound_idx (Inbound_id ASC),
-  INDEX Iid_idx (Inbound_Iid ASC),
+  INDEX Iid_idx (Inbound_Iname ASC),
   INDEX Warehouse_entry_details_Stocks1_idx (Inbound_Stockid ASC),
   CONSTRAINT Inbound_id
     FOREIGN KEY (Inbound_id)
@@ -312,8 +309,8 @@ CREATE TABLE IF NOT EXISTS Inbound_details (
     ON DELETE NO ACTION
     ON UPDATE CASCADE,
   CONSTRAINT Inbound_Iid
-    FOREIGN KEY (Inbound_Iid)
-    REFERENCES Items (Iid)
+    FOREIGN KEY (Inbound_Iname)
+    REFERENCES Items (Iname)
     ON DELETE NO ACTION
     ON UPDATE CASCADE,
   CONSTRAINT Inbound_d_stockid
@@ -326,9 +323,9 @@ ENGINE = InnoDB;
 -- Table History_Stocks
 
 CREATE TABLE IF NOT EXISTS History_Stocks (
-  Stockid CHAR(20) NOT NULL ,
+  Stockid INT NOT NULL ,
   Stocks_Wid INT NULL,
-  Stocks_lid CHAR(15) NULL,
+  Stocks_lname VARCHAR(100) NULL,
   Stockamount FLOAT NULL,
   Stockarea INT NULL,
   Instocktime DATETIME NULL,
@@ -342,20 +339,20 @@ CREATE TABLE IF NOT EXISTS Inner_Trasition (
   Transitionid INT NOT NULL,
   I_T_Wid INT NOT NULL,
   Amount FLOAT NOT NULL,
-  I_T_Iid CHAR(15) NOT NULL,
+  Items_Iname VARCHAR(100) NOT NULL,
   Operate CHAR(1) NULL,
   Time DATETIME NULL,
   PRIMARY KEY (Transitionid),
-  INDEX fk_Inner_trasition_Products1_idx (I_T_Iid ASC),
+  INDEX fk_Inner_trasition_Products1_idx (Items_Iname ASC),
   INDEX I_T_Wid_idx (I_T_Wid ASC),
   CONSTRAINT I_T_Wid
     FOREIGN KEY (I_T_Wid)
     REFERENCES Warehouses (Wid)
     ON DELETE NO ACTION
     ON UPDATE CASCADE,
-  CONSTRAINT I_T_Iid
-    FOREIGN KEY (I_T_Iid)
-    REFERENCES Items (Iid)
+  CONSTRAINT I_T_Iname
+    FOREIGN KEY (Items_Iname)
+    REFERENCES Items (Iname)
     ON DELETE NO ACTION
     ON UPDATE CASCADE)
 ENGINE = InnoDB;
@@ -374,43 +371,43 @@ DROP TRIGGER IF EXISTS Inbound_details_AFTER_INSERT;
 DELIMITER $$
 CREATE TRIGGER Outbound_AFTER_INSERT AFTER INSERT ON Outbound FOR EACH ROW
 BEGIN
-	INSERT INTO Customers_Order_statistics SET Cid=NEW.Customer_Cid,Outbound_id=NEW.Outbound_id;
+	INSERT INTO Customers_Order_statistics SET Outbound_id=NEW.Outbound_id, Cid=NEW.Customer_Cid, CreateTime=New.CreateTime;
 END;
 
 CREATE TRIGGER Outbound_details_AFTER_INSERT AFTER INSERT ON Outbound_details FOR EACH ROW
 BEGIN
-	UPDATE Customer_Order_statistics SET Completetime=NEW.Completetime, Outbound_Amount=NEW.Amount, Money=cast(NEW.Amount AS DECIMAL(10,2))*NEW.Unit_price WHERE Outbound_id=NEW.Outbound_id;
+	UPDATE Customer_Order_statistics SET Money=Money+cast(NEW.Amount AS DECIMAL(10,2))*NEW.Unit_price WHERE Outbound_id=NEW.Outbound_id;
 END;
 
 CREATE TRIGGER Stocks_AFTER_INSERT AFTER INSERT ON Stocks FOR EACH ROW
 BEGIN
- INSERT INTO Stock_collection (Iid, Remain_Amount,Wid) VALUES (NEW.Stocks_Iid, NEW.Stockamount, NEW.Stocks_Wid) ON DUPLICATE KEY UPDATE Remain_Amount=Remain_Amount+NEW.Stockamount;
+ INSERT INTO Stock_collection (Iid, Remain_Amount,Wid) VALUES (NEW.Stocks_Iname, NEW.Stockamount, NEW.Stocks_Wid) ON DUPLICATE KEY UPDATE Remain_Amount=Remain_Amount+NEW.Stockamount;
 END;
 
 CREATE TRIGGER Stocks_BEFORE_UPDATE BEFORE UPDATE ON Stocks FOR EACH ROW
 BEGIN
-	UPDATE Stock_collection set Remain_Amount=Remain_Amount-OLD.Stockamount WHERE Iid=OLD.Stocks_Iid;
+	UPDATE Stock_collection set Remain_Amount=Remain_Amount-OLD.Stockamount WHERE Iid=OLD.Stockid;
 END;
 
 CREATE TRIGGER Stocks_AFTER_UPDATE AFTER UPDATE ON Stocks FOR EACH ROW
 BEGIN
-	UPDATE Stock_collection set Remain_Amount=Remain_Amount+NEW.Stockamount WHERE Iid=OLD.Stocks_Iid;
+	UPDATE Stock_collection set Remain_Amount=Remain_Amount+NEW.Stockamount WHERE Iid=OLD.Stockid;
 END;
 
 CREATE TRIGGER Stocks_BEFORE_DELETE BEFORE DELETE ON Stocks FOR EACH ROW
 BEGIN
-	UPDATE Stock_collection SC SET Remain_Amount=Remain_Amount-OLD.Stockamount WHERE SC.Iid=OLD.Stocks_Iid;
-    INSERT INTO History_Stocks SET Stockid=OLD.stockid, Stocks_Wid=OLD.Stocks_Wid, Stocks_Iid=OLD.Stocks_Iid, Stockamount=OLD.Stockamount, Stockarea=OLD.Stockarea, Instocktime=OLD.Instocktime, Outstocktime=NOW();
+	UPDATE Stock_collection SC SET Remain_Amount=Remain_Amount-OLD.Stockamount WHERE SC.Items_Iname=OLD.Stocks_Iname;
+    INSERT INTO History_Stocks SET Stockid=OLD.stockid, Stocks_Wid=OLD.Stocks_Wid, Stocks_Iname=OLD.Stocks_Iname, Stockamount=OLD.Stockamount, Stockarea=OLD.Stockarea, Instocktime=OLD.Instocktime, Outstocktime=NOW();
 END;
 
 CREATE TRIGGER Inbound_AFTER_INSERT AFTER INSERT ON Inbound FOR EACH ROW
 BEGIN
-	INSERT INTO Suppliers_Order_statistics SET Sid=NEW.Suppliers_Sid, Inbound_id=NEW.Inbound_id;
+	INSERT INTO Suppliers_Order_statistics SET Sid=NEW.Suppliers_Sid, Inbound_id=NEW.Inbound_id, CreateTime=NEW.CreateTime;
 END;
 
 CREATE TRIGGER Inbound_details_AFTER_INSERT AFTER INSERT ON Inbound_details FOR EACH ROW
 BEGIN
-	UPDATE Suppliers_Order_statistics SET Completetime=NEW.Completetime, Inbound_Amount=NEW.Amount, Money=cast(NEW.Amount AS DECIMAL(10,2))*NEW.Unit_Price WHERE Inbound_id=NEW.Inbound_id;
+	UPDATE Suppliers_Order_statistics SET Money=Money+cast(NEW.Amount AS DECIMAL(10,2))*NEW.Unit_Price WHERE Inbound_id=NEW.Inbound_id;
 END; $$
 
 DELIMITER $$
