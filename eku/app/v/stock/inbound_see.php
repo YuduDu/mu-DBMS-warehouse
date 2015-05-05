24aa3fabadd35bbe34d48a0ad87ae15f2292ba27
TimKuan <ghx10086@gmail.com>
2015-05-05 11:58:49 +0800

    add new file

---
+++ eku/app/v/stock/inbound_see.php
@@ -0,0 +1,52 @@
+<h4>入库信息</h4>
+<div class="table-responsive">
+  <table class="table table-striped table-bordered">
+  	<thead>
+  		<tr>
+  			<th>Inbound ID</th>
+  			<th>Create Time</th>
+  			<th>Approver</th>
+        <th>Supplier ID</th>
+        <th>Deliverer</th>
+  		</tr>
+      </thead>
+      <tbody>
+  		<tr>
+  			<td><?php echo $res['Inbound_id']?></td>
+  			<td><?php echo $res['CreateTime']?></td>
+        <td><?php echo $res['Approver_id']?></td>
+        <td><?php echo $res['Suppliers_Sid']?></td>
+        <td><?php echo $res['Deliverer']?></td>
+  		</tr>
+    	</tbody>
+  </table>
+</div>
+
+<h4>入库清单</h4>
+<div class="table-responsive">
+  <table class="table table-striped table-bordered">
+    <thead>
+      <tr>
+        <th>Inbound ID</th>
+        <th>Inbound_Iname</th>
+        <th>Amount</th>
+        <th>Unit_Price</th>
+        <th>Inbound_Stockid</th>
+        <th>Warehouse_Wid</th>
+      </tr>
+      </thead>
+      <tbody>
+      <?php foreach($list as $v) {?>
+      <tr>
+        <td><?php echo $v['Inbound_id']?></td>
+        <td><?php echo $v['Inbound_Iname']?></td>
+        <td><?php echo $v['Amount']?></td>
+        <td><?php echo $v['Unit_Price']?></td>
+        <td><?php echo $v['Inbound_Stockid']?></td>
+        <td><?php echo $v['Warehouse_Wid']?></td>
+      </tr>
+      <?php }?>
+      </tbody>
+  </table>
+</div>
+
---
+++ eku/app/v/stock/outbound_see.php
@@ -0,0 +1,52 @@
+<h4>出库信息</h4>
+<div class="table-responsive">
+  <table class="table table-striped table-bordered">
+  	<thead>
+  		<tr>
+  			<th>Outbound_id</th>
+  			<th>Customer_Cid</th>
+  			<th>Createtime</th>
+        <th>Approver_id</th>
+        <th>Consignee</th>
+  		</tr>
+      </thead>
+      <tbody>
+  		<tr>
+  			<td><?php echo $res['Outbound_id']?></td>
+  			<td><?php echo $res['Customer_Cid']?></td>
+        <td><?php echo $res['Createtime']?></td>
+        <td><?php echo $res['Approver_id']?></td>
+        <td><?php echo $res['Consignee']?></td>
+  		</tr>
+    	</tbody>
+  </table>
+</div>
+
+<h4>出库清单</h4>
+<div class="table-responsive">
+  <table class="table table-striped table-bordered">
+    <thead>
+      <tr>
+        <th>Outbound_id</th>
+        <th>Outbound_Iname</th>
+        <th>Amount</th>
+        <th>Unit_Price</th>
+        <th>Outbound_Stockid</th>
+        <th>Warehouse_Wid</th>
+      </tr>
+      </thead>
+      <tbody>
+      <?php foreach($list as $v) {?>
+      <tr>
+        <td><?php echo $v['Outbound_id']?></td>
+        <td><?php echo $v['Outbound_Iname']?></td>
+        <td><?php echo $v['Amount']?></td>
+        <td><?php echo $v['Unit_price']?></td>
+        <td><?php echo $v['Outbound_Stockid']?></td>
+        <td><?php echo $v['Warehouse_Wid']?></td>
+      </tr>
+      <?php }?>
+      </tbody>
+  </table>
+</div>
+
